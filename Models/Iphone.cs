340771f8a850baeffd5525bd729574e664f8da54
Bruno Bussmeyer <98619218+Bruno-Bussmeyer@users.noreply.github.com>
2024-01-09 21:09:06 -0300

    Update Iphone.cs
    Atualização Iphone

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,24 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    // TODO: Herdar da classe "Smartphone" DIO !!!!!!
+
+    public class Iphone : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        // Construtor da classe Iphone que chama o construtor da classe base
+        public Iphone(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        {
+            
+        }
+        // Sobrescrever o método abstrato da classe base
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no iPhone.");
+            // Lógica específica de instalação para o iPhone
+        }
+
+
+        // TODO: Sobrescrever o método "InstalarAplicativo" DIO!!!!!!
     }
 }
