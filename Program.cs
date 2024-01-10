77de3bedd70ad5f05a960b599f763e6a173dd665
Bruno Bussmeyer <98619218+Bruno-Bussmeyer@users.noreply.github.com>
2024-01-09 21:10:33 -0300

    Update Program.cs
    Atualização classe main

--- Program.cs
+++ Program.cs
@@ -1,3 +1,25 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+namespace DesafioPOO.Models
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Criar uma instância da classe Iphone
+            Iphone meuIphone = new Iphone("123456789", "iPhone X", "ABC123", 128);
+
+            // Chamar métodos da classe base
+            meuIphone.Ligar();
+            meuIphone.ReceberLigacao();
+
+            // Chamar método da classe derivada
+            meuIphone.InstalarAplicativo("WhatsApp");
+
+            meuIphone.Ligar();
+            
+        }
+    }
+}
